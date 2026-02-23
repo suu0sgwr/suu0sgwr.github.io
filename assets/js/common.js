3b718235442229682bdefc5d8d923f01248b3a60
suu0sgwr <t325043@m.chukyo-u.ac.jp>
2026-02-23 21:44:47 +0900

    humberger-kaitei

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,3 +9,22 @@ function loadHTML(id, file) {
 loadHTML("header", "/assets/components/header.html");
 loadHTML("aside", "/assets/components/aside.html");
 loadHTML("footer", "/assets/components/footer.html");
+
+(function($) {
+  var $nav   = $('#navArea');
+  var $btn   = $('.toggle_btn');
+  var $mask  = $('#mask');
+  var open   = 'open';
+
+  $btn.on('click', function() {
+    if (!$nav.hasClass(open)) {
+      $nav.addClass(open);
+    } else {
+      $nav.removeClass(open);
+    }
+  });
+
+  $mask.on('click', function() {
+    $nav.removeClass(open);
+  });
+})(jQuery);
