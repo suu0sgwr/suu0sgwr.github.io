20b7dab64c4aab9d3aef060f604f1daa6cf4ab1d
suu0sgwr <t325043@m.chukyo-u.ac.jp>
2026-02-23 21:39:41 +0900

    modosu

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,22 +9,3 @@ function loadHTML(id, file) {
 loadHTML("header", "/assets/components/header.html");
 loadHTML("aside", "/assets/components/aside.html");
 loadHTML("footer", "/assets/components/footer.html");
-
-(function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open'; // class
-  // menu open close
-  $btn.on( 'click', function() {
-    if ( ! $nav.hasClass( open ) ) {
-      $nav.addClass( open );
-    } else {
-      $nav.removeClass( open );
-    }
-  });
-  // mask close
-  $mask.on('click', function() {
-    $nav.removeClass( open );
-  });
-} )(jQuery);
