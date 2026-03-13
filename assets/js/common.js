a0a42981f6f60434437a8fdc06876b56397f851b
suu0sgwr <t325043@m.chukyo-u.ac.jp>
2026-03-13 17:17:21 +0900

    js update

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,21 +10,33 @@ loadHTML("header", "/assets/components/header.html");
 loadHTML("aside", "/assets/components/aside.html");
 loadHTML("footer", "/assets/components/footer.html");
 
-(function($) {
-  var $nav   = $('#navArea');
-  var $btn   = $('.toggle_btn');
-  var $mask  = $('#mask');
-  var open   = 'open';
+function loadHTML(id, file, callback) {
+  fetch(file)
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+      if (callback) callback(); // HTML読み込み後に実行
+    });
+}
 
-  $btn.on('click', function() {
-    if (!$nav.hasClass(open)) {
-      $nav.addClass(open);
-    } else {
+// header読み込み後にハンバーガーメニューのイベント設定
+loadHTML("header", "/assets/components/header.html", function() {
+  (function($) {
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open';
+
+    $btn.on('click', function() {
+      $nav.toggleClass(open); // toggleClassにまとめると簡潔
+    });
+
+    $mask.on('click', function() {
       $nav.removeClass(open);
-    }
-  });
+    });
+  })(jQuery);
+});
 
-  $mask.on('click', function() {
-    $nav.removeClass(open);
-  });
-})(jQuery);
+// asideとfooterはそのまま
+loadHTML("aside", "/assets/components/aside.html");
+loadHTML("footer", "/assets/components/footer.html");
