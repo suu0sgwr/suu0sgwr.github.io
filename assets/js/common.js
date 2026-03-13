275bee31aa9fefd7ba0a9e75ed768deef637195b
suu0sgwr <t325043@m.chukyo-u.ac.jp>
2026-03-13 19:41:39 +0900

    aside

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,11 +10,20 @@ function loadHTML(id, file, callback) {
 loadHTML("header", "/assets/components/header.html", function() {
   loadHTML("aside", "/assets/components/aside.html", function() {
 
-    const btn   = document.querySelector('.toggle_btn');  // 🍔
-    const aside = document.querySelector('.side-nav');    // aside
-    const mask  = document.getElementById('mask');        // マスク
+const btn   = document.querySelector('.toggle_btn');
+const aside = document.querySelector('.side-nav');
+const mask  = document.getElementById('mask');
 
-    if (!btn || !aside || !mask) return;
+if (btn && aside && mask) {
+  btn.addEventListener('click', () => {
+    aside.classList.toggle('open'); // スマホだけ有効
+    mask.classList.toggle('open');
+  });
+  mask.addEventListener('click', () => {
+    aside.classList.remove('open');
+    mask.classList.remove('open');
+  });
+}
 
     // スマホのみ、aside初期非表示
     aside.style.right = '-250px';
